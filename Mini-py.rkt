43791df9522656f1580b78521c6afa2aa4803f67
Mayra Alejandra Sanchez Salinas <mayra.alejandra.sanchez@correounivalle.edu.co>
2022-12-04 19:49:28 -0500

    Arreglos d ela gramatica

--- Mini-py.rkt
+++ Mini-py.rkt
@@ -14,12 +14,13 @@
 (define especificacion-lexica
   '((espacio-blanco (whitespace) skip)
     (comentario ("#" (arbno (or digit letter #\newline whitespace))) skip)  ;Los comentarios inician con #
+    (letter("'" letter "'") symbol)
     (identificador ( letter (arbno (or letter digit))) symbol) ;Con letter se identifica que puede iniciar con mayuscula o minuscula 
     (numero (digit (arbno digit)) number)
     (numero ("-" digit (arbno digit)) number)
     (numero (digit (arbno digit) "." digit (arbno digit)) number)
     (numero ("-" digit (arbno digit) "." digit (arbno digit)) number)
-    ;(bool ())
+    (bool (or "true" "false") boolean)
     (texto (letter (arbno (or letter ":" "?" "=" "'" "#" "$" "&" "." "," ";" "*" "!" "¡" "¿" "-" "_"))) string)))
 
 ; Gramatica
@@ -39,8 +40,8 @@
   (expresion (identificador) var-exp)
 
   ;Definiciones
-  (expresion ("var" (arbno indentificador "=" (expresion))"," "in" expresion ";")varLet-exp) ;Hacer el manejo de los valores mutables
-  (expresion ("const" (arbno indentificador "=" (expresion))"," "in" expresion ";")const-exp)
+  (expresion ("var" (arbno identificador "=" (expresion))"," "in" expresion ";") varLet-exp) ;Hacer el manejo de los valores mutables
+  (expresion ("const" (arbno identificador "=" (expresion))"," "in" expresion ";")const-exp)
   (expresion ("rec" (arbno identificador "(" (separated-list identificador ",") ")" "=" expresion)  "in" expresion) 
                 rec-exp)
   ;Datos
@@ -50,11 +51,18 @@
   
   ;Constructores de datos predefinidos:
 
-  ;**Faltan los constructores de datos predefinidos
+  (expresion ("[" (separated-list expresion ";") "]") list-exp)
+  (expresion ("tupla" "[" (separated-list expresion ";") "]") tupla-exp) ;preguntar
+  (expresion ("{" (identificador "=" expresion (arbno ";" identificador "=" expresion))) registro-exp) ;preguntar
 
+  (expr-bool (pred-prim "(" expresion "," expresion ")") pred-exp)
+  (expr-bool (oper-bin-bool "(" expr-bool "," expr-bool ")") oper-bin-exp)
+  (expr-bool (bool) bool-exp)
+  (expr-bool (oper-un-bool "(" expr-bool ")" ) oper-un-exp)
+  
+  
   ;Estructura de control
-  (expression ("begin" expresion (arbno ";" expression) "end")
-                begin-exp)
+  (expression ("begin" expresion (arbno ";" expression) "end") begin-exp)
   (expresion ("if" expr-bool "then" expresion ("[" "else" expresion "]")"end") condicional-exp)
   (expresion ("while" expr-bool "do" expresion "done") while-exp)
   (expresion ("for" identificador "=" expresion (or "to" "downto") expresion "do" expresion "done") for-exp)
@@ -72,53 +80,85 @@
   (primitiva-bin ("-") primitiva-resta)
   (primitiva-bin ("/") primitiva-div)
   (primitiva-bin ("*") primitiva-multi)
-  ;(primitiva-bin ("%") primitiva-modulo)
+  (primitiva-bin ("%") primitiva-mod)
 
   ; Primitiva unaria
-  (primitiva-un ("longitud") primitiva-longitud)
   (primitiva-un ("add1") primitiva-add1)
-  (primitiva-un ("sub1") primitiva-sub1)))
+  (primitiva-un ("sub1") primitiva-sub1))
 
 ;--------------------------------Hexadecimales----------------------------------------------------------------------------------
 ; Primitiva binaria 
-  (primitiva-bin ("+") primitiva-suma-ex)
-  (primitiva-bin ("-") primitiva-resta)
-  (primitiva-bin ("*") primitiva-multi)
+  (primitiva-bin ("+") primitiva-suma-hex)
+  (primitiva-bin ("-") primitiva-resta-hex)
+  (primitiva-bin ("*") primitiva-multi-hex)
 
 ; primitiva unaria
-  (primitiva-un ("add1") primitiva-add1)
-  (primitiva-un ("sub1") primitiva-sub1)
+  (primitiva-un ("add1") primitiva-add1-hex)
+  (primitiva-un ("sub1") primitiva-sub1-hex)
+
+;------------------------------------- Operadores booleanos --------------------------------------------------
+  (pred-prim ("<") menor)
+  (pred-prim (">") mayor)
+  (pred-prim ("<=") menor_igual)
+  (pred-prim (">=") mayor_igual)
+  (pred-prim ("==") igual)
+
+  (oper-bin-bool ("and") and-op)
+  (oper-bin-bool ("or") or-op)
+
+  (oper-un-bool ("not") not)
 
 ;---------------------------------------Cadenas------------------------------------------------------------------------------------
  (primitiva-bin ("concat") primitiva-concat)
  (primitiva-un ("longitud") primitiva-longitud)
 
+;--------------------------------------- Primitivas generales ------------------------------------------------
+  (primitiva-un ("vacio?") primitiva-vacio?)
+  (primitiva-un ("vacio") primitiva-vacio)
+  (primitiva-un ("cabeza") primitiva-cabeza)
+  (primitiva-un ("cola") primitiva-cola)
 ;---------------------------------------Listas-----------------------------------------------------------------------------------
-  (primitiva-un ("vacio?") primitiva-vacio?)
-  (primitiva-un ("vacio") primitiva-vacio)
-  (primitiva-un ("crear") primitiva-crear)
-  (primitiva-un ("cabeza") primitiva-cabeza)
-  (primitiva-un ("cola") primitiva-cola)
-  (primitiva-bi ("append") primitiva-append)
+  (primitiva-un ("crear-lista") primitiva-crear-lista)
+  (primitiva-un ("lista?") primitiva-lista)
+  (primitiva-bin ("append") primitiva-append)
   (primitiva-un ("ref-list") primitiva-refList)
-  (primitiva-bi ("set-list") primitiva-setList)
-
-;-----------------------------------Listas/tuplas---------------------------------------------------------------------------------
-  (primitiva-un ("vacio?") primitiva-vacio?)
-  (primitiva-un ("vacio") primitiva-vacio)
-  (primitiva-un ("crear") primitiva-crear)
-  (primitiva-un ("cabeza") primitiva-cabeza)
-  (primitiva-un ("cola") primitiva-cola)
-
+  (primitiva-bin ("set-list") primitiva-setList)
 ;------------------------------------Tuplas---------------------------------------------------------------------------------------
+  (primitiva-un ("crear-tupla") primitiva-crear-tupla)
   (primitiva-un ("tupla?") primitiva-tupla?)
-  (primitiva-bi ("ref-tuple") primitiva-refTuple)
-
+  (primitiva-bin ("ref-tuple") primitiva-refTuple)
 ;-----------------------------------Registros---------------------------------------------------------------------------------------
   (primitiva-un ("registros?") primitiva-registros?)
   (primitiva-un ("crear-resgistro") primitiva-crearRegistro)
-  (primitiva-bi ("ref-resgistro") primitiva-refRegistro)
-  (primitiva-bi ("set-registro") primitiva-setRegistro)
+  (primitiva-bin ("ref-resgistro") primitiva-refRegistro)
+  (primitiva-bin ("set-registro") primitiva-setRegistro))
 
+;------------------------- para el interpretador --------------------------------------------------------------
+; Tipos de datos para la sintaxis abstracta de la gramática construidos automáticamente:
+
+(sllgen:make-define-datatypes especificacion-lexica gramatica)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes especificacion-lexica gramatica)))
+
+;Parser, Scanner, Interfaz
+;El FrontEnd (Análisis léxico (scanner) y sintáctico (parser) integrados)
+
+(define scan&parse
+  (sllgen:make-string-parser especificacion-lexica gramatica))
+
+;El Analizador Léxico (Scanner)
+
+(define just-scan
+  (sllgen:make-string-scanner especificacion-lexica gramatica))
+
+;El Interpretador (FrontEnd + Evaluación + señal para lectura )
+
+(define interpretador
+  (sllgen:make-rep-loop "--> "
+    (lambda (programa) (evaluar-programa  programa))
+    (sllgen:make-stream-parser 
+      especificacion-lexica
+      gramatica)))
 
   
